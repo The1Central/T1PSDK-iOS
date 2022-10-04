3e9afc7c51869cfcc59246d1722a5dfaca16718e
The1-Itsaraporn <t1c20189967@T1C20189967.local>
2022-10-04 21:43:29 +0700

    Add XCFramework file

---
+++ T1PSDK.xcframework/ios-arm64/T1PSDK.framework/Headers/T1PSDK.h
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for T1PSDK.
+FOUNDATION_EXPORT double T1PSDKVersionNumber;
+
+//! Project version string for T1PSDK.
+FOUNDATION_EXPORT const unsigned char T1PSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <T1PSDK/PublicHeader.h>
+
+
